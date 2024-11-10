83758e57ff871e2edf75346e1e8d57b9365c0077
Andre-0405 <andre.wang0405@gmail.com>
2024-11-10 15:29:00 -0800

    Add files via upload
    First Commit!

---
+++ TestScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lambda function
+        //define unit vector 
+        //beta = v/c
+        //gamma = 1/sqrt(1-beta^2)
+        //lambdainxdirection = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
